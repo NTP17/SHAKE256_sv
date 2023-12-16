3dc0ec45e5d631953a43dd8f6f99d2d5a88e1976
Phu Nguyen-Truong <ngtphu0905@gmail.com>
2023-12-16 15:21:44 +0700

    update

--- KECCAK_f.sv
+++ KECCAK_f.sv
@@ -1,31 +1,35 @@
 module KECCAK_f (
-	input  [63:0] A_in  [0:4][0:4],
-	output [63:0] A_out [0:4][0:4]
+	input  [1599:0] S_in,
+	output [1599:0] S_out
 );
 
-	logic [63:0] A0  [0:4][0:4];
-	logic [63:0] A1  [0:4][0:4];
-	logic [63:0] A2  [0:4][0:4];
-	logic [63:0] A3  [0:4][0:4];
-	logic [63:0] A4  [0:4][0:4];
-	logic [63:0] A5  [0:4][0:4];
-	logic [63:0] A6  [0:4][0:4];
-	logic [63:0] A7  [0:4][0:4];
-	logic [63:0] A8  [0:4][0:4];
-	logic [63:0] A9  [0:4][0:4];
-	logic [63:0] A10 [0:4][0:4];
-	logic [63:0] A11 [0:4][0:4];
-	logic [63:0] A12 [0:4][0:4];
-	logic [63:0] A13 [0:4][0:4];
-	logic [63:0] A14 [0:4][0:4];
-	logic [63:0] A15 [0:4][0:4];
-	logic [63:0] A16 [0:4][0:4];
-	logic [63:0] A17 [0:4][0:4];
-	logic [63:0] A18 [0:4][0:4];
-	logic [63:0] A19 [0:4][0:4];
-	logic [63:0] A20 [0:4][0:4];
-	logic [63:0] A21 [0:4][0:4];
-	logic [63:0] A22 [0:4][0:4];
+	logic [63:0] A_in  [0:4][0:4];
+	logic [63:0] A0    [0:4][0:4];
+	logic [63:0] A1    [0:4][0:4];
+	logic [63:0] A2    [0:4][0:4];
+	logic [63:0] A3    [0:4][0:4];
+	logic [63:0] A4    [0:4][0:4];
+	logic [63:0] A5    [0:4][0:4];
+	logic [63:0] A6    [0:4][0:4];
+	logic [63:0] A7    [0:4][0:4];
+	logic [63:0] A8    [0:4][0:4];
+	logic [63:0] A9    [0:4][0:4];
+	logic [63:0] A10   [0:4][0:4];
+	logic [63:0] A11   [0:4][0:4];
+	logic [63:0] A12   [0:4][0:4];
+	logic [63:0] A13   [0:4][0:4];
+	logic [63:0] A14   [0:4][0:4];
+	logic [63:0] A15   [0:4][0:4];
+	logic [63:0] A16   [0:4][0:4];
+	logic [63:0] A17   [0:4][0:4];
+	logic [63:0] A18   [0:4][0:4];
+	logic [63:0] A19   [0:4][0:4];
+	logic [63:0] A20   [0:4][0:4];
+	logic [63:0] A21   [0:4][0:4];
+	logic [63:0] A22   [0:4][0:4];
+	logic [63:0] A_out [0:4][0:4];
+
+	S_to_A string_to_array (.S(S_in), .A(A_in));
 
     Rnd round0  (.A_in(A_in), .A_out(A0),    .i_r(5'd0));
 	Rnd round1  (.A_in(A0),   .A_out(A1),    .i_r(5'd1));
@@ -51,5 +55,7 @@ module KECCAK_f (
 	Rnd round21 (.A_in(A20),  .A_out(A21),   .i_r(5'd21));
 	Rnd round22 (.A_in(A21),  .A_out(A22),   .i_r(5'd22));
 	Rnd round23 (.A_in(A22),  .A_out(A_out), .i_r(5'd23));
+
+	A_to_S array_to_string (.A(A_out), .S(S_out));
 
 endmodule
