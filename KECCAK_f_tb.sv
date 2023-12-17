a9a92e16a298b656885f07b48ed9465be2bd4d2c
Phu Nguyen-Truong <phu.nguyen7591202538@hcmut.edu.vn>
2023-12-18 04:11:40 +0700

    remove comments

--- KECCAK_f_tb.sv
+++ KECCAK_f_tb.sv
@@ -2,28 +2,21 @@
 
 module KECCAK_f_tb;
 
-    // Inputs and Outputs
     logic [1599:0] S_in, S_out;
     
-    // Instantiate the module under test
     KECCAK_f dut (.S_in(S_in), .S_out(S_out));
 
-    // Test stimulus
     initial begin
         $dumpfile("KECCAKf_tb.vcd");
         $dumpvars;
 
-	    // Apply stimulus
         S_in = 'h1F00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
         
-        // Wait for a few clock cycles
         #1;
         
-        // Print the output
         $display("S_in  = %h", S_in);
         $display("S_out = %h", S_out);
 
-        // End the simulation
         $finish;
     end
     
