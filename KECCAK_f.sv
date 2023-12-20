c61cc4ae15c2504cdd2ed564e6fe593026ad5922
Phu Nguyen-Truong <phu.nguyen7591202538@hcmut.edu.vn>
2023-12-20 20:34:24 +0700

    fix

--- KECCAK_f.sv
+++ KECCAK_f.sv
@@ -19,25 +19,20 @@ module KECCAK_f (
 
 	Rnd rnd_inst (.A_in(A_in_mid), .i_r(round), .A_out(A_mid_out));
 
-	genvar x, y;
 	always_ff @(posedge clock, negedge reset)
 		if (!reset)
-			generate
-				for (x = 0; x < 5; x = x + 1) begin : reset_x
-					for (y = 0; y < 5; y = y + 1) begin : reset_y
-						assign A_out[x][y] = 0;
-					end
-				end
-			endgenerate
-
-		else if (round == 24)
-		generate
-			for (x = 0; x < 5; x = x + 1) begin : full_x
-				for (y = 0; y < 5; y = y + 1) begin : full_y
-					assign A_out[x][y] = 0;
+			for (int x = 0; x < 5; x = x + 1) begin : reset_x
+				for (int y = 0; y < 5; y = y + 1) begin : reset_y
+					A_out[x][y] = 0;
 				end
 			end
-		endgenerate
+
+		else if (round == 24)
+			for (int x = 0; x < 5; x = x + 1) begin : full_x
+				for (int y = 0; y < 5; y = y + 1) begin : full_y
+					A_out[x][y] = 0;
+				end
+			end
 
 		else A_out = A_mid_out;
 
