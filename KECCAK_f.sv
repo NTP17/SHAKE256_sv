173fdc2c044e2006f17564efa517372898d31448
Phu Nguyen-Truong <phu.nguyen7591202538@hcmut.edu.vn>
2023-12-20 20:08:14 +0700

    sequential-ization

--- KECCAK_f.sv
+++ KECCAK_f.sv
@@ -1,17 +1,31 @@
 module KECCAK_f (
+	input clock, reset,
 	input  [1599:0] S_in,
-	output [1599:0] S_out
+	output [1599:0] S_out,
+	output done
 );
 
 	logic [63:0] A_in      [0:4][0:4];
 	logic [63:0] A_in_mid  [0:4][0:4];
-	logic [63:0] A_out_mid [0:4][0:4];
+	logic [63:0] A_mid_out [0:4][0:4];
 	logic [63:0] A_out     [0:4][0:4];
+	logic  [4:0] round;
 
 	S_to_A string_to_array (.S(S_in), .A(A_in));
 
+	round_counter rndcnt (.clock(clock), .reset(reset), .round(round));
 
+	assign A_in_mid = (round == 0) ? A_in : A_out;
+
+	Rnd rnd_inst (.A_in(A_in_mid), .i_r(round), .A_out(A_mid_out));
+
+	always_ff @(posedge clock, negedge reset)
+		if (!reset)           A_out = 0;
+		else if (round == 24) A_out = 0;
+		else                  A_out = A_mid_out;
 
 	A_to_S array_to_string (.A(A_out), .S(S_out));
+
+	assign done = (round == 24);
 
 endmodule
--- KECCAK_f_tb.sv
+++ KECCAK_f_tb.sv
@@ -3,19 +3,36 @@
 module KECCAK_f_tb;
 
     logic [1599:0] S_in, S_out;
+    logic clock, reset, done;
     
-    KECCAK_f dut (.S_in(S_in), .S_out(S_out));
+    KECCAK_f dut (.clock(clock), .reset(reset), .S_in(S_in), .S_out(S_out), .done(done));
 
     initial begin
         $dumpfile("KECCAKf_tb.vcd");
         $dumpvars;
 
+        clock = 0;
+        reset = 0;
         S_in = 'h1F00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
-        
-        #1;
-        
+
         $display("S_in  = %h", S_in);
         $display("S_out = %h", S_out);
+        $display("done  = %b", done);
+        $display;
+
+        #1; reset = 1;
+    end
+
+    initial forever #1 clock = ~clock;
+
+    initial begin
+        repeat (24) begin
+            #2;
+            $display("S_in  = %h", S_in);
+            $display("S_out = %h", S_out);
+            $display("done  = %b", done);
+            $display;
+        end
 
         $finish;
     end
