d79709d38a68405c4608504b00fd4521097d476f
Phu Nguyen-Truong <phu.nguyen7591202538@hcmut.edu.vn>
2023-12-26 22:58:51 +0700

    add control unit

---
+++ control_unit.sv
@@ -0,0 +1,11 @@
+module control_unit (
+    input clock, reset,
+    input [10:0] length,
+    output [2:0] addrA, addrB,
+    output [1:0] state_sel,
+    output wren, busy, full, aclr, squeeze
+);
+
+
+
+endmodule
